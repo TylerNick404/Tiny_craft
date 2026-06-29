3a180afd316c39061a95352094b5227d8eda0441
Nick <tylernicky404@gmail.com>
2026-06-29 07:50:20 -0400

    Delete scripts directory

--- scripts/floor.py
+++
@@ -1,9 +0,0 @@
-from ursina import *
-
-def gfloor(floor_texture = '', model_floor = 'block'):
-    for a in range(1, 61):
-        for b in range(1, 61):
-            line1 = Entity(model= model_floor,
-                texture = floor_texture,
-                collider= 'box',
-                position=(a, -8, b))
